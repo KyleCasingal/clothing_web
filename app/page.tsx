2fd0655ce2153e5d3cb57b502b05cd62dc23d35d
Kyle Casingal <kylecasingal36@gmail.com>
2024-01-21 11:34:12 +0800

    navbar layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,37 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>Hello World!</h1>
+    <main>
+      <h3>Hello World!</h3>
+      <h3>Hello World!</h3>
+      <h3>Hello World!</h3>
+      <h3>Hello World!</h3>
+      <h3>Hello World!</h3>
+      <h3>Hello World!</h3>
+      <h3>Hello World!</h3>
+      <h3>Hello World!</h3>
+      <h3>Hello World!</h3>
+      <h3>Hello World!</h3>
+      <h3>Hello World!</h3>
+      <h3>Hello World!</h3>
+      <h3>Hello World!</h3>
+      <h3>Hello World!</h3>
+      <h3>Hello World!</h3>
+      <h3>Hello World!</h3>
+      <h3>Hello World!</h3>
+      <h3>Hello World!</h3>
+      <h3>Hello World!</h3>
+      <h3>Hello World!</h3>
+      <h3>Hello World!</h3>
+      <h3>Hello World!</h3>
+      <h3>Hello World!</h3> <h3>Hello World!</h3>
+      <h3>Hello World!</h3>
+      <h3>Hello World!</h3>
+      <h3>Hello World!</h3>
+      <h3>Hello World!</h3>
+      <h3>Hello World!</h3>
+      <h3>Hello World!</h3>
+      <h3>Hello World!</h3>
+    </main>
   );
 }
