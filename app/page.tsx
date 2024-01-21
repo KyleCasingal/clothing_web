b1bb8e2d19b2721174483d4bbcd07583e4c6a2d8
Kyle Casingal <kylecasingal36@gmail.com>
2024-01-21 12:01:23 +0800

    main layout

--- app/page.tsx
+++ app/page.tsx
@@ -3,36 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <h3>Hello World!</h3>
-      <h3>Hello World!</h3>
-      <h3>Hello World!</h3>
-      <h3>Hello World!</h3>
-      <h3>Hello World!</h3>
-      <h3>Hello World!</h3>
-      <h3>Hello World!</h3>
-      <h3>Hello World!</h3>
-      <h3>Hello World!</h3>
-      <h3>Hello World!</h3>
-      <h3>Hello World!</h3>
-      <h3>Hello World!</h3>
-      <h3>Hello World!</h3>
-      <h3>Hello World!</h3>
-      <h3>Hello World!</h3>
-      <h3>Hello World!</h3>
-      <h3>Hello World!</h3>
-      <h3>Hello World!</h3>
-      <h3>Hello World!</h3>
-      <h3>Hello World!</h3>
-      <h3>Hello World!</h3>
-      <h3>Hello World!</h3>
-      <h3>Hello World!</h3> <h3>Hello World!</h3>
-      <h3>Hello World!</h3>
-      <h3>Hello World!</h3>
-      <h3>Hello World!</h3>
-      <h3>Hello World!</h3>
-      <h3>Hello World!</h3>
-      <h3>Hello World!</h3>
-      <h3>Hello World!</h3>
+      
     </main>
   );
 }
